68ab2f8ad7dc42d9ff821d033cb8fe15988841bd
Josias Besodia <djodev23@gmail.com>
2025-03-21 10:10:55 +0100

    robot config

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -10,7 +12,11 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', [
+  path.join(__dirname, 'views'),
+  path.join(__dirname, 'includes'),
+  path.join(__dirname, 'views/admin')
+]);
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
