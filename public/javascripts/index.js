24b4f56c5013240fcbbee53b995645d04772ec55
Josias Besodia <djodev23@gmail.com>
2025-03-22 06:44:27 +0100

    change13

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,8 +4,12 @@ function navbarShowH (param) {
     const navbar_part2 = document.getElementById('navbar_part2');
 
     if(param === "open" && fa_xmark_menu && fa_bars_menu && navbar_part2) {
-
+        alert('open')
+        navbar_part2.classList.remove('hidden');
+        navbar_part2.classList.add('flex');
     } else if (param === "close" && fa_xmark_menu && fa_bars_menu && navbar_part2) {
-        
+        alert('close')
+    } else {
+        console.log('Valeur inconnue');
     }
 }
