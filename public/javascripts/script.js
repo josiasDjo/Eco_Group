75530350833eadf3fec950b4c04dac3e236c2c34
Josias Besodia <djodev23@gmail.com>
2025-03-22 05:53:43 +0100

    change4

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,2 +1,5 @@
 
-gsap.from(['.logo_nav','.text_navbar_annim'], { duration: 0.6, delay: 0.2, opacity: 0, stagger: 0.2});
+gsap.from(['.logo_nav','.text_navbar_annim', '.text_heroHeader', '.btn_HeroHeader'], { 
+    duration: 0.6, delay: 0.2, opacity: 0, stagger: 0.2
+});
+gsap.from('', {});
