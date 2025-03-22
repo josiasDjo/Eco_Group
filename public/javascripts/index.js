956a46e28a0bd34ca43c8d7f3fbc929a26aa1101
Josias Besodia <djodev23@gmail.com>
2025-03-22 06:40:06 +0100

    change12

---
+++ public/javascripts/index.js
@@ -0,0 +1,11 @@
+function navbarShowH (param) {
+    const fa_xmark_menu = document.getElementById('fa_xmark_menu');
+    const fa_bars_menu = document.getElementById('fa_bars_menu');
+    const navbar_part2 = document.getElementById('navbar_part2');
+
+    if(param === "open" && fa_xmark_menu && fa_bars_menu && navbar_part2) {
+
+    } else if (param === "close" && fa_xmark_menu && fa_bars_menu && navbar_part2) {
+        
+    }
+}
