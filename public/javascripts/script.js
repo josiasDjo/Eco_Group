24e1f2170826540c769395e762d72d8c845fbe5f
Josias Besodia <djodev23@gmail.com>
2025-03-22 05:49:39 +0100

    change3

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,2 @@
-import { gsap } from "gsap";
 
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
-
-gsap.from('text_navbar_annim', { duration: 3, delay: 1, ease: 'bounce', opacity: 0, stagger: 5});
+gsap.from(['.logo_nav','.text_navbar_annim'], { duration: 0.6, delay: 0.2, opacity: 0, stagger: 0.2});
