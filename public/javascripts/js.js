e0581503f9705daae0d73c2d460fd2801b73608a
Josias Besodia <djodev23@gmail.com>
2025-03-26 22:20:56 +0100

    change62

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -1,1 +1,6 @@
 const newusersignup = document.getElementById('newusersignup');
+if (newusersignup) {
+    newusersignup.addEventListener('submit', async () => {
+        alert('Submited')
+    })
+}
