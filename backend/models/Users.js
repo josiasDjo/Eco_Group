0eade6f883ecc80ba4f7347608c0ea70d0d85600
Josias Besodia <djodev23@gmail.com>
2025-03-26 19:53:19 +0100

    change19

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -2,7 +2,11 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('./index');
 
 const User = sequelize.define('table_users', {
-
+    user_id: {},
+    fist_name: {},
+    last_name: {},
+    email: {},
+    password: {}
 },{
     timestamps: true
 })
