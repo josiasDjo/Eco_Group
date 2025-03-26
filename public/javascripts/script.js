6249839ab398363b520ffba0da0b24d35b86cac8
Josias Besodia <djodev23@gmail.com>
2025-03-26 23:30:40 +0100

    change14

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,11 @@
 
-gsap.from(['.logo_nav','.text_navbar_annim', '.text_heroHeader', '.btn_HeroHeader'], { 
-    duration: 0.6, delay: 0.2, opacity: 0, stagger: 0.2
-});
-gsap.from('.svg_inProgress', { opacity:0, duration: 0.8, delay: 1.5, x:'-100%'});
+// gsap.from(['.logo_nav','.text_navbar_annim', '.text_heroHeader', '.btn_HeroHeader'], { 
+//     duration: 0.6, delay: 0.2, opacity: 0, stagger: 0.2
+// });
+// gsap.from('.svg_inProgress', { opacity:0, duration: 0.8, delay: 1.5, x:'-100%'});
 
-gsap.to(".spinner_svg", {
-    rotation: "+=360",duration: 2, ease: "none", repeat: -1, repeatDelay: 0.02  
-});
+// gsap.to(".spinner_svg", {
+//     rotation: "+=360",duration: 2, ease: "none", repeat: -1, repeatDelay: 0.02  
+// });
 
-gsap.to('.svg_bounce', { duration: 0.6, y:-20, ease:"bounce.out", yoyo: true, repeat: -1});
+// gsap.to('.svg_bounce', { duration: 0.6, y:-20, ease:"bounce.out", yoyo: true, repeat: -1});
