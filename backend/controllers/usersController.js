c0f6b3d1ebffb692d21ceb109ed0f1c595c734ac
Josias Besodia <djodev23@gmail.com>
2025-03-26 20:09:56 +0100

    change24

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -3,7 +3,10 @@ const User = require('../models/Users');
 exports.createUser = async (req, res) => {
     try {
         const { fist_name,last_name,email,password	} = req.body;
-        
+        const emailExist = await User.findOne({where: {email}});
+        if (emailExist) return res.json({ success: false, message: 'Cet email existe déjà !!'});
+
+        await User.create({fist_name,last_name,email,password})
     } catch (err) {
         console.log('Une erreur s\'est produite : ', err);
         return res.json({ success: false, message: 'Une erreur s\'est produite'});
