231011e7b980ad7aef7d1943dbecb4fd23a9d489
Josias Besodia <djodev23@gmail.com>
2025-03-26 20:52:30 +0100

    change44

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -5,7 +5,7 @@ const User = sequelize.define('table_users', {
     user_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     fist_name: { type: DataTypes.STRING, allowNull: false },
     last_name: { type: DataTypes.STRING, allowNull: false },
-    email: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.TEXT, allowNull: false }
 },{
     timestamps: true
