e2a7466002ff951bbae7bba3c45e6de23b48dc98
Josias Besodia <djodev23@gmail.com>
2025-03-26 20:05:58 +0100

    change23

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -2,7 +2,8 @@ const User = require('../models/Users');
 
 exports.createUser = async (req, res) => {
     try {
-
+        const { fist_name,last_name,email,password	} = req.body;
+        
     } catch (err) {
         console.log('Une erreur s\'est produite : ', err);
         return res.json({ success: false, message: 'Une erreur s\'est produite'});
