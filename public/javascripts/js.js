0f458919d48990899dee0b0a5fee471a0334bc1f
Josias Besodia <djodev23@gmail.com>
2025-03-26 22:29:22 +0100

    change65

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -1,6 +1,12 @@
 const newusersignup = document.getElementById('newusersignup');
 if (newusersignup) {
-    newusersignup.addEventListener('submit', async () => {
-        alert('Submited')
+    newusersignup.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        alert('Submited');
+
+        const first_name = document.getElementById('firstName');
+        const last_name = document.getElementById('lastName');
+        const email = document.getElementById('email');
+        const password = document.getElementById('password');
     })
 }
