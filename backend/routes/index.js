0e987f592d35d751053b6d4426b2b80f1e7c4ea3
Josias Besodia <djodev23@gmail.com>
2025-03-26 22:39:09 +0100

    change71

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index');
-// });
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
 
 // Get connexion page
-router.get('/', (req, res) => {
+router.get('/login', (req, res) => {
   console.log('Page de connexion');
   res.render('signup');
   // return res.sendFile("../../custom/signin.html");
