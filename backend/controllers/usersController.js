e255309932359f84991db0104646ded7499b0cdc
Josias Besodia <djodev23@gmail.com>
2025-03-26 20:49:01 +0100

    change42

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -71,5 +71,10 @@ exports.resetPassword = async (req, res) => {
 }
 
 exports.deleteAccount = async (req, res) => {
-    
+    try {
+        
+    } catch(err) {
+        console.log('Une erreur s\'est produite : ', err);
+        return res.json({ success: false, message: 'Une erreur s\'est produite'});
+    }
 }
