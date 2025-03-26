4414e728657601883535cb461f5f30adf28fb5a6
Josias Besodia <djodev23@gmail.com>
2025-03-26 21:19:26 +0100

    node module copy files

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -4,7 +4,7 @@ const usersController = require('../controllers/usersController');
 
 router.post('/new-user', usersController.createUser);
 router.get('/', usersController.getUser);
-router.put('/update/user', usersController, usersController.resetPassword);
+router.put('/update/user', usersController.resetPassword);
 router.put('/reset-my-password', usersController.updateUser);
 router.delete('/delete/myAccount', usersController.deleteAccount);
 
