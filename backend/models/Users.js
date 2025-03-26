8c33fb1e9a7f266b2e7e5a1dc16df4d4917f950b
Josias Besodia <djodev23@gmail.com>
2025-03-26 19:55:21 +0100

    change20

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -2,11 +2,13 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('./index');
 
 const User = sequelize.define('table_users', {
-    user_id: {},
-    fist_name: {},
-    last_name: {},
-    email: {},
-    password: {}
+    user_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    fist_name: { type: DataTypes.STRING, allowNull: false },
+    last_name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false },
+    password: { type: DataTypes.TEXT, allowNull: false }
 },{
     timestamps: true
 })
+
+module.exports = User;
