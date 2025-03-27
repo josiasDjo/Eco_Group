ff666f42249abbf9e466a9d5b70f0c9f999b2e4c
Josias Besodia <djodev23@gmail.com>
2025-03-27 19:26:56 +0100

    models set success without testing

---
+++ backend/models/responses.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./index');
+
+const Response = sequelize.define("table_response", {
+    response_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    comment_id: { type: DataTypes.INTEGER, allowNull: false }, 
+    user_id: { type: DataTypes.INTEGER, allowNull: false }, 
+    content: { type: DataTypes.TEXT, allowNull: false },
+}, {
+    timestamp: true
+})
+
+module.exports = Response;
