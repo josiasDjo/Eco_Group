f6b68978e74d8a8f43b692b9550d16a8de3ef887
Josias Besodia <djodev23@gmail.com>
2025-03-27 06:30:26 +0100

    change22

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -36,6 +36,7 @@ exports.getUser = async (req, res) => {
         let password_hash = userExist.password;
         const saltRounds = 10;
         const verify_password = await bcrypt.compare(password,password_hash);
+        console.log('Password : ', verify_password);
         if(verify_password == true) {
             console.log('Mot de passe incorrect')
             return res.json({ success: false, message: 'Mot de passe incorrect'});
