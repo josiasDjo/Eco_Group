7edb9308529f9681b0db8e1399ea1abd8a55b3e1
Josias Besodia <djodev23@gmail.com>
2025-03-27 10:43:08 +0100

    change35

--- backend/middlewares/authenticateToken.js
+++ backend/middlewares/authenticateToken.js
@@ -1,8 +1,9 @@
 const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req,res,next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+    // const authHeader = req.headers['authorization'];
+    const token = req.cookies.token;
+    // const token = authHeader && authHeader.split(' ')[1];
 
     if (!token) {
         return res.redirect('/login');
