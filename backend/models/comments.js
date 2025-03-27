8e0d046ab09dfb272afe136ecc84c4dd76d02dd1
Josias Besodia <djodev23@gmail.com>
2025-03-27 18:44:18 +0100

    change2

--- backend/models/comments.js
+++ backend/models/comments.js
@@ -2,5 +2,12 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('./index');
 
 const Comment = sequelize.define("table_comments", {
-    
+    comment_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },	
+    author: { type: DataTypes.STRING, allowNull: false }, 	
+    content: { type: DataTypes.TEXT, allowNull: false }, 	
+    statut: { type: DataTypes.STRING, allowNull: true }
+}, {
+    timestamp: true
 })
+
+module.exports = Comment;
