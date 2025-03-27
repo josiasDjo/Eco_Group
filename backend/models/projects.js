de82826007c23b28cb45defc4e3e7c117a19e19e
Josias Besodia <djodev23@gmail.com>
2025-03-27 18:58:31 +0100

    change5

---
+++ backend/models/projects.js
@@ -0,0 +1,14 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./index');
+
+const Project = sequelize.define("table_projects", {
+    project_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.TEXT, allowNull: false },
+    start_date: { type: DataTypes.DATE, allowNull: false },
+    end_date: { type: DataTypes.DATE, allowNull: true},
+}, {
+    timestamp: true
+})
+
+module.exports = Project;
