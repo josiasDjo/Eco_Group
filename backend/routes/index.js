02d3754ea05ecd26b1983d7f8ebf079aa3eb1b99
Josias Besodia <djodev23@gmail.com>
2025-03-27 10:58:51 +0100

    change37

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const isAuthenticated = require('../middlewares/isAuthenticated');
 const authenticateToken = require('../middlewares/authenticateToken');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,8 +13,8 @@ router.get('/', function(req, res, next) {
 // Get connexion page
 router.get('/login', (req, res) => {
   console.log('Page de connexion');
-  res.render('sign');
-  // return res.sendFile("../../custom/signin.html");
+  // res.render('sign');
+  res.sendFile(path.join(__dirname, '../../custom/signin.html'));
 });
 
 router.get('/s/admin', isAuthenticated, authenticateToken, (req, res) => {
