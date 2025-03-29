d5b265cbe7685524ee378c55613d63216dbc399e
Josias Besodia <djodev23@gmail.com>
2025-03-29 13:31:07 +0100

    change2

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -69,7 +69,7 @@ function openAddProject(param) {
 
 function openAddToTeam(param) {
     alert('Modal add person');
-    const modals_addFields = document.getElementById('modals_addFields');
+    const modals_addFields = document.getElementById('modals_addMember');
     const addTeamId = document.getElementById('addTeamId');
 
     if(param === 'close') {
