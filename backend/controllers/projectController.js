cc9b5cdd8c5bb1153e2d6a7ffa54eda68129bb81
Josias Besodia <djodev23@gmail.com>
2025-03-29 05:34:00 +0100

    change9

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -3,6 +3,7 @@ const Project = require('../models/projects');
 exports.addProjects = async (req, res) => {
     try { 
         const { title,description,start_date,end_date, image } = req.body;
+        const projectExist = await Project.findOne({where: {title: title}});
     } catch {
         console.log('Une erreur d\'est produite : ', err);
         return res.json({ success: false, message: 'Erreur serveur, réesayer plus tard !! '});
