1ccfda80174cb3f53c21833edca89054b5243ce4
Josias Besodia <djodev23@gmail.com>
2025-03-29 05:50:23 +0100

    change19

--- backend/routes/projectRoute.js
+++ backend/routes/projectRoute.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const projectController = require('../controllers/projectController');
 
-router.post('addProject', projectController);
-router.get('/', projectController);
-router.put('update-project', projectController);
-router.delete('delete-project', projectController);
+router.post('addProject', projectController.addProjects);
+router.get('/', projectController.getAllProjects);
+router.put('update-project', projectController.updateProjects);
+router.delete('delete-project', projectController.deleteProjects);
 
 module.exports = router;
