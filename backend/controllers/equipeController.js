94d391561c185e542a1763afeff9a7c14c8216f4
Josias Besodia <djodev23@gmail.com>
2025-03-29 05:17:17 +0100

    backend config

---
+++ backend/controllers/equipeController.js
@@ -0,0 +1,26 @@
+const { where } = require('sequelize');
+const Equipe = require('../models/equipe');
+
+exports.addToTeam = async (req, res) => {
+    try {
+        const { first_name,last_name,image } = req.body;
+        const newMember = await Equipe.create({first_name,last_name,image});
+        return res.json({ success: true, message: 'Membre ajouté'});
+    } catch (err) {
+        console.log('Une erreur d\'est produite : ', err);
+        return res.json({ success: false, message: 'Erreur serveur, réesayer plus tard !! '});
+    }
+}
+
+exports.updateMember = async (req, re) => {
+    try {
+        const { equipe_id,first_name,last_name,image } = req.body;
+        const memberExist = await Equipe.findByPk(equipe_id);
+        if (!memberExist) return res.json({ success: false, message: 'Membre introuvable'});
+        await Equipe.update({first_name,last_name,image}, {where: {equipe_id:equipe_id}});
+        return res.json({ success: true, message: false });
+    } catch(err) {
+        console.log('Une erreur d\'est produite : ', err);
+        return res.json({ success: false, message: 'Erreur serveur, réesayer plus tard !! '});
+    }
+}
