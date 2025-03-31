d3eec57ae92e605a2e8d7b8b40f003b90f5ab78e
Josias Besodia <djodev23@gmail.com>
2025-03-31 12:06:03 +0200

    change1

--- backend/models/index.js
+++ backend/models/index.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        dialect: 'postgres',
+        dialect: 'mysql',
         logging: false,
         pool: {
             max: 20,
